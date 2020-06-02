e5d80975ad3c969871c48a1abed7f3c04e4cca1d
timniky <63619369+timniky@users.noreply.github.com>
2020-06-02 13:47:21 +0300

    Create luch.h

---
+++ luch.h
@@ -0,0 +1,20 @@
+#pragma once
+ #ifndef Ray_h
+  #define Ray_h
+   #include "vect.h"
+
+class Ray {
+public:
+	Vec3 origin;
+	Vec3 direction;
+
+	Ray(Vec3 ori, Vec3 dir) : origin(ori), direction(dir) {}
+
+	Vec3 get_point(double t) const {
+		return origin + direction * t;
+	}
+	Vec3 reflect_by(const Vec3& normal) const {
+		return direction - normal * normal.dot(direction) * 2;
+	}
+};
+#endif
